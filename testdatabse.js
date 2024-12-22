fdabb7cd78f6933274b8e2f2660842cb1c202394
Rishi <141233915+realrishiiscool@users.noreply.github.com>
2024-12-22 21:56:25 +0530

    Update testdatabse.js

--- testdatabse.js
+++ testdatabse.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '2004', // Make sure this matches the password for root
+    password: '1234', // Make sure this matches the password for root
     port: 3306
 });
 
